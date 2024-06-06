060a3915cb8d08188426518ce2b4ac9ebf4c63b7
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2024-06-06 12:20:20 +0530

    added env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       port: 5432,
       username: process.env.DB_USER_NAME,
       password: process.env.DB_USER_PASSWORD,
-      database: 'nestjs_crud',
+      database: process.env.DB_NAME,
       entities: [User, WalletAddress],
       synchronize: true,
     }),
